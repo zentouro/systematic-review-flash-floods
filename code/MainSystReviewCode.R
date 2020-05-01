1311d27cec5ba9b3f74e364a2d8b31107ecbc2f1
daarcher5 <daarcher5@gmail.com>
2020-05-01 01:34:33 -0400

    screened 25

--- code/MainSystReviewCode.R
+++ code/MainSystReviewCode.R
@@ -24,7 +24,8 @@ library(rmdformats)
 
 rm(list=ls())
 # adjust this to the appropriate folder on your system
-setwd("~/Documents/GitHub/systematic-review-flash-floods")
+setwd("C:\\Users\\Damian\\Documents\\GitHub\\systematic-review-flash-floods")
+#"~/Documents/GitHub/systematic-review-flash-floods"
 # if you're on a Windows PC try: 
 # setwd("C:\\Documents\\GitHub\\systematic-review-flash-floods"")
 # may need to adjust to your specific machine
